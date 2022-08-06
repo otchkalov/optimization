8b0e97b37f118e1ed116d11ecb96ababc735bf7c
Oleksii Chkalov <tchkalov@gmail.com>
2022-08-06 20:50:01 +0300

    Plotter for other one-doim methods added

---
+++ OneDim/Other/plotter.py
@@ -0,0 +1,15 @@
+'''
+    Plot demo graph for one-dim search algorithms
+'''
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+#Plot the objective function
+def plot_graph(x, y, approx, xl, xr):
+    clear_output(wait=True)
+    
+    #plot function graph
+    plt.plot(x, y)
+    plt.plot(x, approx)
+    plt.plot([xl, xr],[0,0],'k')
+    plt.show()
