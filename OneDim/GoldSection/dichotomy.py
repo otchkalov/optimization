c030109d8349fe0563b7d335d0a8f4b1e9c7653b
Oleksii Chkalov <tchkalov@gmail.com>
2022-08-10 12:58:56 +0300

    Dichotomy bug fixes

--- OneDim/GoldSection/dichotomy.py
+++ OneDim/GoldSection/dichotomy.py
@@ -10,13 +10,13 @@ def func(x):
 
 plot_graph = False
 
-xLeft  = 0.5
+xLeft = 0.5
 xRight = 2.0
 
 eps = 1.e-5
 
 if plot_graph:
-    #Generate point for objective function graph      
+    # Generate point for objective function graph
     x = np.linspace(xLeft, xRight, 100)
     y = func(x)
 
@@ -26,7 +26,7 @@ print('  i           x_1           x_m           x_2         f_1         f_m    
 print('---------------------------------------------------------------------------------------------------')
 
 
-# Start dochotomy process
+# Start dichotomy process
 length = xRight - xLeft
 x1 = xLeft + 0.25*length
 x2 = xLeft + 0.75*length
@@ -51,7 +51,7 @@ for i in range(20):
         xm = x2
         fm = f2
     else:
-        # exclude 2 peripherial quarters
+        # exclude 2 peripheral quarters
         xLeft = x1
         xRight = x2
     # Note: we never have to recalculate OF value for new xm
@@ -60,14 +60,13 @@ for i in range(20):
     x2 = xLeft + 0.75*length
     f1 = func(x1)
     f2 = func(x2)
-        
 
-    print(f'{i:3d}  {x1:12.5f}  {xm:12.5f}  {x2:12.5f}  {f1:12.5f}  {fm:12.5f}  {f2:12.5f}  {length:12.5f}')
+    print(f'{i+1:3d}  {x1:12.5f}  {xm:12.5f}  {x2:12.5f}  {f1:12.5f}  {fm:12.5f}  {f2:12.5f}  {length:12.5f}')
     if plot_graph:
         plotter.plot_graph3(x, y, func, xLeft, xRight, x1, x2, xm)
         time.sleep(1)
     if length < eps:
         break
 
-if (i == 19):
-    print('Warning: Required accuracy cannot be reached in 20 steps')
+    if (i == 19):
+        print('Warning: Required accuracy cannot be reached in 20 steps')
--- OneDim/GoldSection/plotter.py
+++ OneDim/GoldSection/plotter.py
@@ -4,67 +4,68 @@
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
-#Plot the objective function
+
+# Plot the objective function
 def plot_graph(x, y, func, xl, xr, x1, x2):
     clear_output(wait=True)
     
-    #plot function graph
+    # plot function graph
     plt.plot(x, y)
-    plt.plot([xl, xr],[0,0],'k')
+    plt.plot([xl, xr], [0, 0], 'k')
     
-    #plot x1 point
-    plt.plot(x1,func(x1),'ro',label='x1')
-    plt.plot([x1,x1],[0,func(x1)],'k')
+    # plot x1 point
+    plt.plot(x1,func(x1), 'ro', label='x1')
+    plt.plot([x1, x1], [0, func(x1)], 'k')
     
-    #plot x2 point
-    plt.plot(x2,func(x2),'bo',label='x2')
-    plt.plot([x2,x2],[0,func(x2)],'k')
+    # plot x2 point
+    plt.plot(x2, func(x2), 'bo', label='x2')
+    plt.plot([x2, x2], [0, func(x2)], 'k')
     
-    #plot xl line
-    plt.plot([xl,xl],[0,func(xl)])
-    plt.annotate('xl',xy=(xl-0.01,-0.2))
+    # plot xl line
+    plt.plot([xl, xl], [0, func(xl)])
+    plt.annotate('xl', xy=(xl-0.01, -0.2))
         
-    #plot xr line
-    plt.plot([xr,xr],[0,func(xr)])
-    plt.annotate('xu',xy=(xr-0.01,-0.2))
+    # plot xr line
+    plt.plot([xr, xr], [0, func(xr)])
+    plt.annotate('xu', xy=(xr-0.01, -0.2))
         
-    #plot x1 line
-    plt.plot([x1,x1],[0,func(x1)],'k')
-    plt.annotate('x1',xy=(x1-0.01,-0.2))
+    # plot x1 line
+    plt.plot([x1, x1], [0, func(x1)], 'k')
+    plt.annotate('x1', xy=(x1-0.01, -0.2))
         
-    #plot x2 line
-    plt.plot([x2,x2],[0,func(x2)],'k')
-    plt.annotate('x2',xy=(x2-0.01,-0.2))
+    # plot x2 line
+    plt.plot([x2, x2], [0, func(x2)], 'k')
+    plt.annotate('x2', xy=(x2-0.01, -0.2))
     
     plt.show()
 
 
-#Plot the objective function
+# Plot the objective function
 def plot_graph3(x, y, func, xl, xr, x1, x2, xm):
     clear_output(wait=True)
     
-    #plot function graph
+    # plot function graph
     plt.plot(x, y)
-    plt.plot([xl, xr],[0,0],'k')
+    plt.plot([xl, xr], [0, 0], 'k')
     
-    #plot x1 point
-    plt.plot(x1,func(x1),'ro',label='x1')
-    plt.plot([x1,x1],[0,func(x1)],'k')
+    # plot x1 point
+    plt.plot(x1, func(x1), 'ro', label='x1')
+    plt.plot([x1, x1], [0, func(x1)], 'k')
     
-    #plot x2 point
-    plt.plot(x2,func(x2),'bo',label='x2')
-    plt.plot([x2,x2],[0,func(x2)],'k')
+    # plot x2 point
+    plt.plot(x2, func(x2), 'bo', label='x2')
+    plt.plot([x2, x2], [0, func(x2)], 'k')
          
-    #plot xm point
-    plt.plot(xm,func(xm),'go',label='xm')
-    plt.plot([xm,xm],[0,func(xm)],'k')
+    # plot xm point
+    plt.plot(xm, func(xm), 'go', label='xm')
+    plt.plot([xm, xm], [0, func(xm)], 'k')
     
-    #plot xl line
-    plt.plot([xl,xl],[0,func(xl)])
-    plt.annotate('xl',xy=(xl-0.01,-0.2))
+    # plot xl line
+    plt.plot([xl, xl], [0, func(xl)])
+    plt.annotate('xl', xy=(xl-0.01, -0.2))
         
-    #plot xr line
-    plt.plot([xr,xr],[0,func(xr)])
-    plt.annotate('xr',xy=(xr-0.01,-0.2))
+    # plot xr line
+    plt.plot([xr, xr], [0, func(xr)])
+    plt.annotate('xr', xy=(xr-0.01, -0.2))
         
-    plt.show() 
+    plt.show()
