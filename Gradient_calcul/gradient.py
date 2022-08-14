149c27d32738a226255640ead38a07b08b27fa92
Oleksii Chkalov <tchkalov@gmail.com>
2022-08-14 21:34:34 +0300

    Contour plot enchanced

--- Gradient_calcul/gradient.py
+++ Gradient_calcul/gradient.py
@@ -6,33 +6,38 @@ def func(x):
     # Calculate object function value
 
     # Simple quadratic function
-    fun = (x[0] - 1) ** 2 + (x[1] - 1) ** 2 - x[0] * x[1]
+    # fun = (x[0] - 1) ** 2 + (x[1] - 1) ** 2 - x[0] * x[1]
     # fun = fun + np.random.uniform(-1, 1)
-    x1Lim = [-4, 4]
-    x2Lim = [-4, 4]
+    # x1Lim = [-4, 4]
+    # x2Lim = [-4, 4]
+    # levels = [-1.9, 0, 8, 16, 24, 32, 40, 48, 56]
 
     # Himmelblau function
     # fun = (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
     # x1Lim = [-5, 5]
     # x2Lim = [-5, 5]
+    # levels = [1.0, 16, 32, 48, 96, 200]
 
     # Rastrigin function
-    # fun = x[0]**2 + x[1]**2 - 10*(2 - np.cos( 2 * np.pi * x[0]) - np.cos( 2 * np.pi * x[1]))
-    # x1Lim = [-1, 1]
-    # x2Lim = [-1, 1]
+    fun = x[0]**2 + x[1]**2 - 10*(2 - np.cos( 2 * np.pi * x[0]) - np.cos( 2 * np.pi * x[1]))
+    x1Lim = [-5, 5]
+    x2Lim = [-5, 5]
+    levels = [0.01, 0.5, 10., 20., 30., 40]
 
     # Rosenbrock function
     # fun = 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
     # x1Lim = [-2, 2]
     # x2Lim = [-1, 3]
+    # levels = [1.0, 16, 32, 48, 96, 200]
 
     # Ackley function
     # fun = -20 * np.exp( -0.2 * np.sqrt( 0.5 * (x[0]**2 + x[1]**2))) - \
     #     np.exp( 0.5 * (np.cos( 2 * np.pi * x[0]) + np.cos( 2 * np.pi * x[1]))) + np.e + 20
     # x1Lim = [-4, 4]
     # x2Lim = [-4, 4]
+    # levels = [1.0, 2, 4, 8, 16, 32]
 
-    return fun, x1Lim, x2Lim
+    return fun, x1Lim, x2Lim, levels
 
 
 def agradient(x):
@@ -49,13 +54,14 @@ def agradient(x):
 
 
 # Objective function gradient plot
-nPoints = 20
-ObjFun, x1Lim, x2Lim = func([0, 0])  # Just to get limits
+nPoints = 200
+ObjFun, x1Lim, x2Lim, levels = func([0, 0])  # Just to get limits
 x1list = np.linspace(x1Lim[0], x1Lim[1], nPoints)
 x2list = np.linspace(x2Lim[0], x2Lim[1], nPoints)
 X1, X2 = np.meshgrid(x1list, x2list)
-ObjFun, x1Lim, x2Lim = func([X1, X2])
+ObjFun, x1Lim, x2Lim, levels = func([X1, X2])
 gradOf = agradient([X1, X2])
+print(ObjFun)
 
-# plotter.plot_graph(X1, X2, ObjFun)
-plotter.vector_plot(X1, X2, ObjFun, gradOf)
+# plotter.plot_graph(X1, X2, ObjFun, levels)
+plotter.vector_plot(X1, X2, ObjFun, gradOf, levels)
--- Gradient_calcul/plotter.py
+++ Gradient_calcul/plotter.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 
 # Plot the objective function
-def plot_graph(x1, x2, objFun):
+def plot_graph(x1, x2, objFun, levels):
     fig, ax = plt.subplots(1, 1)
-    cp = ax.contour(x1, x2, objFun)
+    cp = ax.contour(x1, x2, objFun, levels)
     fig.colorbar(cp)  # Add a colorbar to a plot
     ax.set_title('Object Function Contour Plot')
     ax.set_xlabel('x_1')
@@ -16,11 +16,12 @@ def plot_graph(x1, x2, objFun):
 
 
 # Plot a vector field of object function gradient 
-def vector_plot(x1, x2, ObjFun, gradOf):
+def vector_plot(x1, x2, objFun, gradOf, levels):
     # clear_output(wait=True)
 
     fig, ax = plt.subplots(1, 1)
-    cp = ax.contour(x1, x2, ObjFun)
+    # cp = ax.contour(x1, x2, ObjFun)
+    cp = ax.contour(x1, x2, objFun, levels)
     fig.colorbar(cp)  # Add a colorbar to a plot
     plt.quiver(x1, x2, gradOf[0], gradOf[1])
     ax.set_title('Object Function Antigradient')
