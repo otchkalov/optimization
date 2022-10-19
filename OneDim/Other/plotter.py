19b6c27f32ef05b102e59be3f96b124d982a4f1d
Oleksii Chkalov <ochkalov@volvocars.com>
2022-10-19 16:17:25 +0300

    Code refactoring

--- OneDim/Other/plotter.py
+++ OneDim/Other/plotter.py
@@ -2,12 +2,10 @@
     Plot demo graph for one-dim search algorithms
 '''
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
 
 #Plot the objective function
 def plot_graph(x, y, approx, xl, xr):
-    clear_output(wait=True)
-    
+
     #plot function graph
     plt.plot(x, y)
     plt.plot(x, approx)
