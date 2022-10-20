318a3d6997d74d33fd8e9a947b52a97659ecfc0f
Oleksii Chkalov <tchkalov@gmail.com>
2022-10-20 21:56:27 +0300

    Random search

--- Random_search/Free_set/random_search.py
+++ Random_search/Free_set/random_search.py
@@ -23,31 +23,54 @@ def griewank(xx):
 
     return (summ - prodd + 1) * 1.e3
 
+def schwefel(xx):
+    # SCHWEFEL FUNCTION
+    # Search interval[-500; 500]
+    # Global minimum f(x) = 0 at x = [420.9687; 420.9687]
+    # INPUT:
+    # xx = [x1, x2]
+
+    global number_of_estimations
+    number_of_estimations = number_of_estimations + 1
+
+    summ = 0
+    for ii in range(2):
+        summ = summ + xx[ii] * np.sin(np.sqrt(np.abs(xx[ii])))
+
+    return 418.9829 * 2 - summ
+
 
 number_of_estimations = 0
-func = 'griewank'
-x_min = [-5, -5]
-x_max = [5, 5]
-width = [x_max[0] - x_min[0], x_max[1] - x_min[1]]
-arg_abs_min = [0.0, 0.0]
 
-# func = 'schwefel'
-# x_min = [-500, -500]
-# x_max = [ 500,  500]
-# width = [x_max[0] - x_min[0], x_max[1] - x_min[1]]
-# arg_abs_min = [420.9687, 420.9687]
+# func = 'griewank'
+func = 'schwefel'
 
-n_points = 50
+if func == 'griewank':
+    x_min = np.array([-5, -5])
+    x_max = np.array([5, 5])
+    arg_abs_min = np.array([0.0, 0.0])
+else:
+    x_min = np.array([-500, -500])
+    x_max = np.array([500, 500])
+    arg_abs_min = np.array([420.9687, 420.9687])
 
-list_of_args = [[0, 0] for i in range(n_points)]
+n_points = 500
+
+list_of_args = np.zeros([n_points + 1, 2])
 obj_fun = np.zeros(n_points)
 
 minimal = np.inf
 best_point = 0
+width = np.subtract(x_max, x_min)
 
 for i in range(n_points):
-    list_of_args[i] = [x_min[0] + random() * width[0], x_min[1] + random() * width[1]]
-    obj_fun[i] = griewank(list_of_args[i])
+    # We need to call random() twice to get different values for 2 arguments
+    delta = np.array([random(), random()])
+    list_of_args[i] = np.add(x_min, np.multiply(delta, width))
+    if func == 'griewank':
+        obj_fun[i] = griewank(list_of_args[i])
+    else:
+        obj_fun[i] = schwefel(list_of_args[i])
     if obj_fun[i] < minimal:
         minimal = obj_fun[i]
         best_point = i
@@ -60,9 +83,14 @@ distance = np.sqrt((list_of_args[best_point][0] - arg_abs_min[0])**2 +
                    (list_of_args[best_point][1] - arg_abs_min[1])**2)
 print(f'Distance {distance:12.5g}')
 
-x_opt = coordinate_descent(griewank, list_of_args[best_point], 1.e-5)
-f_opt = griewank(x_opt)
-list_of_args.append(x_opt)
+if func == 'griewank':
+    x_opt = coordinate_descent(griewank, list_of_args[best_point], 1.e-5)
+    f_opt = griewank(x_opt)
+else:
+    x_opt = coordinate_descent(schwefel, list_of_args[best_point], 1.e-5)
+    f_opt = schwefel(x_opt)
+
+list_of_args[n_points] = x_opt
 
 print(f'Optimal solution found: [{x_opt[0]:12.5g}  {x_opt[1]:12.5g}] of = {f_opt:12.5g}')
 print(f'Total number of OF estimations: {number_of_estimations:5d}')
@@ -70,7 +98,7 @@ print('-----------------------------------------------')
 
 
 # Plot contours of the function and evaluated points
-n_ticks = 100
+n_ticks = 500
 x1_list = np.linspace(x_min[0], x_max[0], n_ticks)
 x2_list = np.linspace(x_min[1], x_max[1], n_ticks)
 obj_fun_grid = [np.zeros(n_ticks) for i in range(n_ticks)]
@@ -79,4 +107,4 @@ for i in range(n_ticks):
     for j in range(n_ticks):
         obj_fun_grid[i][j] = griewank([x1_list[i], x2_list[j]])
 
-plotter.plot_graph(x1_list, x2_list, obj_fun_grid, list_of_args, best_point)
+plotter.plot_graph_p1(x1_list, x2_list, obj_fun_grid, list_of_args, best_point)
